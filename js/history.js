83b0044e43554a61747dc23bc49537f1c1086e65
popyakter121 <popy2000akter@gmail.com>
2024-09-23 22:27:40 +0800

    added history button click go history page donate button click go donate button page

---
+++ js/history.js
@@ -0,0 +1,18 @@
+function donateHistory() {
+  return `
+    <div class="container mx-auto rounded-lg">
+      <h3 class="text-lg">
+        <span id=""></span>
+        Taka is Donated for Donate for
+        <span id=""></span>
+      </h3>
+      <p class="p-2">
+        <span id=""></span>
+      </p>
+    </div>
+  `
+}
+
+function showTime(){
+  
+}
--- js/home.js
+++ js/home.js
@@ -1,4 +1,44 @@
+function accessId(id){
+  return document.getElementById(id);
+}
 
+
+// History page to Donate Page
+document.getElementById('history-page').addEventListener('click', function(){
+  const donateSection = document.getElementById('history-content');
+  donateSection.classList.remove('hidden');
+
+  const contentContainer = document.getElementById('content-container');
+  contentContainer.classList.add('hidden');
+  
+
+  const historyButtonBg = document.getElementById('history-page');
+  historyButtonBg.classList.add('bg-primary')
+
+  const donateButtonBg = document.getElementById('donate-page');
+  donateButtonBg.classList.remove('bg-primary')
+  
+});
+
+
+// Donate Page to History Page
+document.getElementById('donate-page').addEventListener('click', function(){
+  const donateSection = document.getElementById('history-content');
+  donateSection.classList.add('hidden');
+  const contentContainer = document.getElementById('content-container');
+  contentContainer.classList.remove('hidden');
+  contentContainer.classList.remove('bg-primary');
+  
+
+  const historyButtonBg = document.getElementById('history-page');
+  historyButtonBg.classList.remove('bg-primary')
+
+  const donateButtonBg = document.getElementById('donate-page');
+  donateButtonBg.classList.add('bg-primary')
+});
+
+
+// card 1
 document.getElementById('Donate-now-btn1').addEventListener('click', function(){
   const donateValue1 = document.getElementById('donate-value1').value;
   const donateValueNumber1 = parseFloat(donateValue1);
@@ -12,12 +52,84 @@ document.getElementById('Donate-now-btn1').addEventListener('click', function(){
     alert('Please Donate Positive Amount');
   }
 
-  const balance = document.getElementById('balance').innerText;
-  const balanceNumber = parseFloat(balance);
-  console.log(balanceNumber)
+  const balance = document.getElementById('balance');
+  const balanceValue = balance.innerText;
+  const balanceNumber = parseFloat(balanceValue);
 
-  
   const currentBalance = balanceNumber - donateValueNumber1;
-  console.log(currentBalance);
-  balance.innerText = currentBalance;
+  accessId('balance').innerText = currentBalance;
+
+
+  const donatedMoney1 = accessId('donated-money1');
+  const donatedMoneyValue1 = donatedMoney1.innerText;
+  const donatedMoneyNumber1 =  parseFloat(donatedMoneyValue1);
+  const donatedCurrentMoney1 = donateValueNumber1 + donatedMoneyNumber1;
+  
+  accessId('donated-money1').innerText = donatedCurrentMoney1;
+  
+});
+
+
+
+// card 2
+document.getElementById('Donate-now-btn2').addEventListener('click', function(){
+  const donateValue2 = document.getElementById('donate-value2').value;
+  const donateValueNumber2 = parseFloat(donateValue2);
+  
+  if(isNaN(donateValueNumber2)) {
+    alert("Please Donate Valid Amount");
+    return;
+  }
+
+  if(donateValueNumber2 < 0){
+    alert('Please Donate Positive Amount');
+  }
+
+  const balance = document.getElementById('balance');
+  const balanceValue = balance.innerText;
+  const balanceNumber = parseFloat(balanceValue);
+
+  const currentBalance = balanceNumber - donateValueNumber2;
+  accessId('balance').innerText = currentBalance;
+
+
+  const donatedMoney2 = accessId('donated-money2');
+  const donatedMoneyValue2 = donatedMoney2.innerText;
+  const donatedMoneyNumber2 =  parseFloat(donatedMoneyValue2);
+  const donatedCurrentMoney2 = donateValueNumber2 + donatedMoneyNumber2;
+  
+  accessId('donated-money2').innerText = donatedCurrentMoney2;
+  
+});
+
+
+
+// card 3
+document.getElementById('Donate-now-btn3').addEventListener('click', function(){
+  const donateValue3 = document.getElementById('donate-value3').value;
+  const donateValueNumber3 = parseFloat(donateValue3);
+  
+  if(isNaN(donateValueNumber3)) {
+    alert("Please Donate Valid Amount");
+    return;
+  }
+
+  if(donateValueNumber3 < 0){
+    alert('Please Donate Positive Amount');
+  }
+
+  const balance = document.getElementById('balance');
+  const balanceValue = balance.innerText;
+  const balanceNumber = parseFloat(balanceValue);
+
+  const currentBalance = balanceNumber - donateValueNumber3;
+  accessId('balance').innerText = currentBalance;
+
+
+  const donatedMoney3 = accessId('donated-money3');
+  const donatedMoneyValue3 = donatedMoney3.innerText;
+  const donatedMoneyNumber3 =  parseFloat(donatedMoneyValue3);
+  const donatedCurrentMoney3 = donateValueNumber3 + donatedMoneyNumber3;
+  
+  accessId('donated-money3').innerText = donatedCurrentMoney3;
 });
