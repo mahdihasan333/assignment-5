6cb78fc28184118c9d57861da8c461048bf5f1f2
popyakter121 <popy2000akter@gmail.com>
2024-09-23 19:52:45 +0800

    commit

--- js/home.js
+++ js/home.js
@@ -1,29 +1,23 @@
-function id(id) {
-  return document.getElementById(id);
-};
 
 document.getElementById('Donate-now-btn1').addEventListener('click', function(){
-  const donateValue1 = id('donate-value1').value;
-  const donateNumber1 = parseFloat(donateValue1);
-
-  if(isNaN(donateNumber1)) {
-    alert('Please Donate Valid Amount');
-    return;
-  }
-  if(donateNumber1 <= 0){
-    alert('Please Donate Positive Amount');
-    return;
-  }
-
-  const currentBalance = id('current-balance').innerText;
-  const updateCurrentBalance = currentBalance - donateValue1;
-
-  if(updateCurrentBalance < 0) {
-    alert("Not enough Balance You Can't Donate");
-    return;
-  }
+  const donateValue1 = document.getElementById('donate-value1').value;
+  const donateValueNumber1 = parseFloat(donateValue1);
   
-  const donatedMoney1 = id('donated-money').innerText;
-  const donatedMoneyNumber1 = parseFloat(donatedMoney1);
-  const donationFinal = donateValue1 + donatedMoneyNumber1;
+  if(isNaN(donateValueNumber1)) {
+    alert("Please Donate Valid Amount");
+    return;
+  }
+
+  if(donateValueNumber1 < 0){
+    alert('Please Donate Positive Amount');
+  }
+
+  const balance = document.getElementById('balance').innerText;
+  const balanceNumber = parseFloat(balance);
+  console.log(balanceNumber)
+
+  
+  const currentBalance = balanceNumber - donateValueNumber1;
+  console.log(currentBalance);
+  balance.innerText = currentBalance;
 });
